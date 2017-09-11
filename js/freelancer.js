1e59c350dfcc11697ae433cc0fc50f8b565a5732
lwgray <lwgray@gmail.com>
2017-09-11 17:41:08 -0400

    Initial Jekyll template

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,3 +1,9 @@
+/*!
+ * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('.page-scroll a').bind('click', function(event) {
@@ -24,3 +30,8 @@ $(function() {
 $('body').scrollspy({
     target: '.navbar-fixed-top'
 })
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
